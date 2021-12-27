4fc640010bc8f5ff22599afa3c10b99ed97d9b76
Your Name <you@example.com>
2021-12-27 20:56:59 +0200

    added authguard

--- WebShio_FrontEnd/src/app/app-routing.module.ts
+++ WebShio_FrontEnd/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 import { CategoryComponent } from './admin/category/category.component';
 import { EditItemComponent } from './admin/edit-item/edit-item.component';
 import { ViewItemsComponent } from './admin/view-items/view-items.component';
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { CartComponent } from './cart/cart.component';
@@ -16,12 +17,32 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shopping-cart', component: CartComponent },
   { path: 'item/:itemId', component: SingleItemComponent },
-  { path: 'admin', component: AdminHomeComponent },
-  { path: 'admin/categories', component: CategoryComponent },
-  { path: 'admin/add-item', component: AddItemComponent },
-  { path: 'admin/edit-item/:itemId', component: EditItemComponent },
-  { path: 'admin/items', component: ViewItemsComponent },
-  { path: 'admin/add-category', component: AddCategoryComponent },
+  { path: 'admin', component: AdminHomeComponent, canActivate: [AuthGuard] },
+  {
+    path: 'admin/categories',
+    component: CategoryComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'admin/add-item',
+    component: AddItemComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'admin/edit-item/:itemId',
+    component: EditItemComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'admin/items',
+    component: ViewItemsComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'admin/add-category',
+    component: AddCategoryComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: SignupComponent },
   { path: '**', redirectTo: '' },
