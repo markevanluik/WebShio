39ed81bbc34eb29b368eb3046857cf2d9c3e6d42
Your Name <you@example.com>
2022-01-04 12:02:51 +0200

    Refactored ErrorHandler

--- WebShio_BackEnd/src/main/java/ee/mark/webshiospring/exception/ErrorHandler.java
+++ WebShio_BackEnd/src/main/java/ee/mark/webshiospring/exception/ErrorHandler.java
@@ -3,43 +3,41 @@ package ee.mark.webshiospring.exception;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.NoSuchElementException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler()
-    public ResponseEntity<ErrorResponse> handleException(NoSuchElementException e) {
-        ErrorResponse response = new ErrorResponse(new Date(), "This resource does not exist");
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleException(NoSuchElementException e) {
+        return new ErrorResponse(new Date(), "This resource does not exist");
     }
 
     @ExceptionHandler()
-    public ResponseEntity<ErrorResponse> handleException(EmptyResultDataAccessException e) {
-        ErrorResponse response = new ErrorResponse(new Date(), "This resource does not exist");
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleException(EmptyResultDataAccessException e) {
+        return new ErrorResponse(new Date(), "This resource does not exist");
     }
 
     @ExceptionHandler()
-    public ResponseEntity<ErrorResponse> handleException(RegistrationException e) {
-        ErrorResponse response = new ErrorResponse(new Date(), "Mandatory fields are not filled");
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleException(RegistrationException e) {
+        return new ErrorResponse(new Date(), "Mandatory fields are not filled");
     }
 
     @ExceptionHandler()
-    public ResponseEntity<ErrorResponse> handleException(UserExistsException e) {
-        ErrorResponse response = new ErrorResponse(new Date(), "User with this person code already exists");
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleException(UserExistsException e) {
+        return new ErrorResponse(new Date(), "User with this person code already exists");
     }
 
     @ExceptionHandler()
-    public ResponseEntity<ErrorResponse> handleException(EmailExistsException e) {
-        ErrorResponse response = new ErrorResponse(new Date(), "User with this email already exists");
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleException(EmailExistsException e) {
+        return new ErrorResponse(new Date(), "User with this email already exists");
     }
 }
